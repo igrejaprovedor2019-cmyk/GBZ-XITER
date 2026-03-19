9fd5177f5a8f50fdecbfb6cbfade0332fe8cee7d
igrejaprovedor2019-cmyk <igrejaprovedor2019@gmail.com>
2026-03-19 15:20:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,10 +31,45 @@ client.on('messageCreate', async (message) => {
   if (message.content === '!gbzstar') {
 
     const embed = new EmbedBuilder()
-      .setTitle('🔥😈 Adquira seu Painel ANDROID 😈🔥')
-      .setDescription('💎 Clique abaixo para comprar')
+      .setTitle('🔥😈 Adquira seu Painel FFH4X ANDROID 😈🔥')
+      .setDescription(`
+🔥😈 **Adquira Já seu Painel FFH4X ANDROID** 😈🔥  
+
+🔥 **FFH4X RAGE ANDROID!**  
+
+Se você quer xitar com qualidade e ter resultado, o nosso painel foi feito pra você.  
+
+💎 Produto desenvolvido para entregar eficiência máxima e uma experiência diferenciada.  
+
+🔥 **O que ele tem?**  
+• Aimbot Full - Configurável  
+• ESPs Full - Configurável  
+• AimBot - Configurável  
+• Puxa para cabeça, pescoço e peito  
+
+💥 **Diferenciais**  
+• Funciona em todas versões do Android 🚀  
+• Tutorial e suporte para instalação  
+
+🎮 **Ideal para quem quer:**  
+• Jogar AP ✅  
+• Jogar modo rank ✅  
+• Jogar CS rank ✅  
+
+📥 **O que você recebe:**  
+• Uma Key no privado  
+• Acesso a canais de tutorial e download  
+
+🚨 Painel Rage, ou seja, há risco de blacklist  
+
+📦 Entrega rápida no privado  
+📲 Suporte antes da compra  
+
+😈🔥 **Não perde tempo… garante o seu agora!** 🔥😈
+      `)
       .setImage('https://media.discordapp.net/attachments/1482528899903782932/1484254280088027216/file_000000008530720eb8922a615208f883.png')
-      .setColor(0x00ff88);
+      .setColor(0x00ff88)
+      .setFooter({ text: 'GBZ STORE 🔥' });
 
     const botao = new ButtonBuilder()
       .setCustomId('abrir_ticket')
@@ -60,10 +95,10 @@ client.on('interactionCreate', async (interaction) => {
       .setCustomId('produto')
       .setPlaceholder('Escolha seu plano')
       .addOptions([
-        { label: '1 dia', description: 'R$17,90', value: '17.90' },
-        { label: '7 dias', description: 'R$25,90', value: '25.90' },
-        { label: '10 dias', description: 'R$35,90', value: '35.90' },
-        { label: '30 dias', description: 'R$55,90', value: '55.90' }
+        { label: 'Android - 1 dia', description: 'R$17,90', value: '17.90' },
+        { label: 'Android - 7 dias', description: 'R$25,90', value: '25.90' },
+        { label: 'Android - 10 dias', description: 'R$35,90', value: '35.90' },
+        { label: 'Android - 30 dias', description: 'R$55,90', value: '55.90' }
       ]);
 
     return interaction.reply({
